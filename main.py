f232231e7ce9c19bc0fb175ded38b03fef6afc19
YoBunnyTiger <157778892+YoBunnyTiger@users.noreply.github.com>
2024-07-03 21:04:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+
+print((9 ** 0.5) * 5)
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+print((1234 % 1000) // 10 + (5678 % 1000) // 10)
+
+# 4th program
+
+print(round(13.42) == (42.13 * 100 % 100) or round(42.13) == (13.42 * 100 % 100))
