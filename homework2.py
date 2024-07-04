fa2b26ab7ab4b81e493cbb8262cc8509499d09ec
YoBunnyTiger <157778892+YoBunnyTiger@users.noreply.github.com>
2024-07-05 00:02:18 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,9 @@
-name = "Nikita"
-print('Name: ' + name)
-age = 36
-print('Age: ' + str(age))
-newAge = age + 1
-print('New Age: ' + str(newAge))
-is_Student = True
-print('Is Student: ' + str(is_Student))
+number_of_completed_homeworks = 12
+number_of_hours_spent = 1.5
+name_of_the_course = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_homeworks
+
+print('Курс: ', name_of_the_course, ', всего задач: ',
+      number_of_completed_homeworks, ', затрачено часов: ',
+      number_of_hours_spent, ', среднее время выполнения: ',
+      time_for_one_task, ' часа.', sep='')
