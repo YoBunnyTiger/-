5d314b6f5aaabcf32eb42ef13789d9cb57014a55
nksound <nksound@mail.ru>
2024-07-07 06:32:07 +0300

    update

--- homework2.py
+++ homework2.py
@@ -3,7 +3,7 @@ number_of_hours_spent = 1.5
 name_of_the_course = 'Python'
 time_for_one_task = number_of_hours_spent / number_of_completed_homeworks
 
-print('Курс: ', name_of_the_course, ', всего задач: ',
-      number_of_completed_homeworks, ', затрачено часов: ',
-      number_of_hours_spent, ', среднее время выполнения: ',
-      time_for_one_task, ' часа.', sep='')
+print(f'Курс: {name_of_the_course}, '
+      f'всего задач: {number_of_completed_homeworks}, '
+      f'затрачено часов: {number_of_hours_spent}, '
+      f'среднее время выполнения: {time_for_one_task} часа.')
